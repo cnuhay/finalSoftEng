5d5870c56880947e09823408889907b87403ae4d
cnuhay <144130728+cnuhay@users.noreply.github.com>
2024-08-10 12:36:23 -0700

    first Push

---
+++ src/main/java/com/example/finalsofteng/FinalSoftEngApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalsofteng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalSoftEngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalSoftEngApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalsofteng/FinalSoftEngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalsofteng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalSoftEngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
